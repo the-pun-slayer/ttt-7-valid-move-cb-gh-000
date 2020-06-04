8d0bdf9b3faa1727b65e2a2fdffab71bd20420d7
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-04 16:09:57 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,14 @@
 # code your #valid_move? method here
 def valid_move?(board,index)
-  if index.between?(-1,9)
+  if index.between?(-1,9){
     if board[index]==" " || board[index]=="" || board[index]== nil
       return truthiness
     else
       return falsey
+    end
   else
     return falsey
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
